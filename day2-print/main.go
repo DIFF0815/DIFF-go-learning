991122c509a3d379b5154873085ba071ffedf1dd
liangyi0815 <941334185@qq.com>
2021-06-01 18:24:03 +0800

    提交初识变量

--- day2-print/main.go
+++ day2-print/main.go
@@ -32,13 +32,49 @@ func main()  {
 	//fmt.Println("1"=="01")
 
 	//变量
-	var st string = "中国人"
-	fmt.Println(st)
-	var sd int = 123
-	fmt.Println(sd)
-	var flag bool
-	flag = true
-	fmt.Println(flag)
+	//var st string = "中国人"
+	//fmt.Println(st)
+	//var sd int = 123
+	//fmt.Println(sd)
+	//var flag bool
+	//flag = true
+	//fmt.Println(flag)
+
+	//var name string
+	//fmt.Scanf("%s",&name)
+	//fmt.Println(name)
+	//if name == "中国人"{
+	//	fmt.Println("输入正确")
+	//}else {
+	//	fmt.Println("输入错误")
+	//}
+
+	//变量简写
+	var name string = "lisi"
+	fmt.Println(name)
+	var name2 = "zhangsan"
+	fmt.Println(name2)
+	name3 := "wangwu"
+	fmt.Println(name3)
+
+	var name4,name5,name6 string
+	name4 = "4"
+	name5 = "5"
+	name6 = "6"
+	fmt.Println(name4,name5,name6)
+
+	var (
+		name7 = "7"
+		name8 bool
+		name9 = 12345
+		name10 string //默认 ""
+		name11 int  //默认 0
+		name12 bool //默认 false
+	)
+	name8 = true
+	fmt.Println(name7,name8,name9,name10,name11,name12)
+
+
 
 
 
