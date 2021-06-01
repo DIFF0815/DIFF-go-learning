384adfecba481d340ff58ae65cab7d65ef3a8f1b
liangyi0815 <941334185@qq.com>
2021-06-01 17:55:26 +0800

    提交

---
+++ day2-print/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main()  {
+	////输出
+	//print("我是print输出")
+	//print("我是print输出2")
+	//print("\n")
+	//println("我是println输出")
+	//println("我是println输出2")
+
+	//fmt.Print("我是fmt.Print输出")
+	//fmt.Print("我是fmt.Print输出2")
+	//fmt.Print("\n")
+	//fmt.Println("我是fmt.Println输出")
+	//fmt.Println("我是fmt.Println输出2")
+	//fmt.Println("我是fmt","Println输出3")
+	//fmt.Printf("我是%s,现在在%s,%d岁,身高比例%.2f,是一个100%%好人","diff","gz",18,20.234)
+
+
+	//数据类型
+	//fmt.Println(123)
+	//////fmt.Println("1"+2) //不能这样加 会报错
+	//fmt.Println("1" + "2")
+	//fmt.Println(1+2)
+	//
+	//fmt.Println(1==1)
+	//fmt.Println(1>2)
+	//fmt.Println(1<2)
+	//fmt.Println("Fa"=="fa")
+	//fmt.Println("1"=="01")
+
+	//变量
+	var st string = "中国人"
+	fmt.Println(st)
+	var sd int = 123
+	fmt.Println(sd)
+	var flag bool
+	flag = true
+	fmt.Println(flag)
+
+
+
+}
